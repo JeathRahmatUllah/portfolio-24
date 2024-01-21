b6737f7c90aa37e7991c451acc0ae5939caf238b
JeathRahmatUllah <cbridoy@gmail.com>
2024-01-21 21:45:11 +0600

    skills v1

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Skill } from '../../../types';
+import skillsData from "../../../../data/skills.json"
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+  skills : Skill[] = skillsData
 }
